3b9f49702f79552863dd3cd91c6279dd64d7f212
NickKononenko <kononenko0812@gmail.com>
2023-09-01 00:13:58 +0300

    header added

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,8 @@
+const toggle = document.querySelector('.toggle');
+const body = document.querySelector('body');
+toggle.addEventListener('click', onToggleClick);
 
+function onToggleClick() {
+  toggle.classList.toggle('active');
+  body.classList.toggle('dark-body-theme');
+}
