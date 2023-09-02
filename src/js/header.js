579852c5c5a03b0b2ec8634675a2fd67569e8c2e
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-09-02 17:40:49 +0200

    mobile-first-page.v2

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,19 @@
 const toggle = document.querySelector('.toggle');
 const body = document.querySelector('body');
+const openBtnMenu = document.querySelector('.burger-btn');
+const closeBtnMenu = document.querySelector('.burger-btn-close');
+
 toggle.addEventListener('click', onToggleClick);
+
+openBtnMenu.addEventListener('click', () => {
+  openBtnMenu.classList.add('js-switch-btn');
+  closeBtnMenu.classList.remove('js-switch-btn');
+});
+
+closeBtnMenu.addEventListener('click', () => {
+  openBtnMenu.classList.remove('js-switch-btn');
+  closeBtnMenu.classList.add('js-switch-btn');
+});
 
 function onToggleClick() {
   toggle.classList.toggle('active');
