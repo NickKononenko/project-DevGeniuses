0939c9370c2e9b9d50c2890bb930e4516ffe5c60
olekskhom <oleks.khom@gmail.com>
2023-09-04 21:38:45 +0300

    body

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,1 +1,7 @@
 console.log('test')
+
+const inputTheme = document.querySelector('.div-theme');
+const spanTheme = document.querySelector('.span-theme');
+
+const body = document.querySelector('body');
+let indexTheme = false;
