eb6a114c1b854c86c8748d69f3359b9d3ca29fec
olekskhom <oleks.khom@gmail.com>
2023-09-04 21:43:58 +0300

    addEventListener

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -5,3 +5,17 @@ const spanTheme = document.querySelector('.span-theme');
 
 const body = document.querySelector('body');
 let indexTheme = false;
+
+
+inputTheme.addEventListener('change', () => {
+  if (indexTheme) {
+    indexTheme = false;
+    localStorage.setItem('userTheme', 'light');
+  } else {
+    indexTheme = true;
+    localStorage.setItem('userTheme', 'dark');
+  }
+
+  currentTheme();
+});
+
