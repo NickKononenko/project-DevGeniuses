4402c7d0b456e89d92b6f6834be718eecdf4d4b0
Ryndina Olena <ryndinaolena@gmail.com>
2023-09-05 22:07:05 +0300

    fixed

--- src/js/categories.js
+++ src/js/categories.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import renderingByCategory from './renderingByCategory';
 const category_list = document.querySelector('.nav-categories-list');
 
+
 async function getCategoryList() {
   const { data } = await axios.get(
     'https://books-backend.p.goit.global/books/category-list'
@@ -10,6 +11,9 @@ async function getCategoryList() {
 }
 
 const renderCategories = async () => {
+  if (!category_list) {
+    return
+  }
   try {
     const category = await getCategoryList();
     category_list.innerHTML = await markupCategoriesList(category);
@@ -27,11 +31,15 @@ const renderCategories = async () => {
     });
   } catch (error) {
     console.log('Oops! Something went wrong');
+
   }
 };
 
 renderCategories();
-category_list.addEventListener('click', checkCategory);
+if (category_list) {
+  category_list.addEventListener('click', checkCategory);
+}
+
 
 function checkCategory(e) {
   if (e.target.dataset.id) {
@@ -43,10 +51,10 @@ function markupCategoriesList(categories) {
   return `<li class="nav-category-item active" data-id="all-categories">
         All categories</li>
         ${categories
-          .map(
-            category => `<li class="nav-category-item" data-id="${category.list_name}">
+      .map(
+        category => `<li class="nav-category-item" data-id="${category.list_name}">
         ${category.list_name}
         </li>`
-          )
-          .join('')}`;
+      )
+      .join('')}`;
 }
